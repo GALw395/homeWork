725a9a805217239cf939293a919499d1c658b736
GALw395 <galw395@mail.ru>
2024-08-21 15:45:05 +0300

    hw3-4v3(correctSort)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,24 +7,18 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { 
+            let sortState = state.slice()
 
             if (action.payload === 'up') {
-
-                let sortState = state.sort((a, b) => a.name.localeCompare(b.name))
-                return sortState
-
-            } else if (action.payload === 'down') {
-
-                let sortState = state.sort((a, b) => a.name.localeCompare(b.name)).reverse()
-                return sortState
-
+                return sortState.sort((a, b) => a.name.localeCompare(b.name))
+            } else if (action.payload === 'down') {                
+                return sortState.sort((a, b) => a.name.localeCompare(b.name)).reverse()
             }
 
             return state 
         }
-        case 'check': {            
-            let sortState = state.filter((a) => a.age >= 18 ).sort((a, b) => a.age - b.age)
-            return sortState
+        case 'check': { 
+            return state.filter(a => a.age >= 18).sort((a, b) => a.age - b.age)
         }
         default:
             return state
